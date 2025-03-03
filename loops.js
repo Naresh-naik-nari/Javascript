17961da95a9409b4c50a394c2266e3f76b07f378
Naresh Naik <nn313570@gmail.com>
2025-03-03 22:23:59 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,46 @@
+// Q-1 Write a for loop to print numbers from 1 to 10.
+for(var a=1;a<=10;a++){
+    console.log(a)
+}
+
+// Q-2 Write a for loop to calculate the sum of all numbers from 1 to 100
+var sum=0;
+for(var n=1;n<=100;n++){
+ sum=n*(n+1)/2
+}
+
+console.log(sum)
+// Q-3 Write a for loop to print the multiplication table of 7 (up to 10)
+num=5
+for(i=1;i<=10;i++){
+    console.log(`${num}x${i}=${num*i}`)
+}
+
+// Q-4 Write a for loop to print all even numbers between 1 and 20
+for(var i=1;i<=20;i++){
+    if(i%2==0){
+        console.log(i)
+    }
+}
+
+// Q-5 Write a for loop to calculate the factorial of a number (e.g., 5!).
+var sum=1
+for(var i=1;i<=5;i++){
+    sum=sum*i
+}
+
+console.log(sum)
+// Q-6 Write a for loop to calculate the power of a number (e.g., 2^5)
+var sum=2
+for(var i=5;i<=5;i++){
+    console.log(sum**i)
+}
+
+// Q-7 Write a for loop to print all odd numbers between 20 and 1 in reverse order
+for(var i=1;i<=20;i++){
+    if(i%2==!0){
+        console.log(i)
+    }
+}
+
+
